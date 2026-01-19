a0527084001c5a7ab38bc46b5d27d3507b189aa7
Craigy131 <65000316+Trey16Miller@users.noreply.github.com>
2026-01-19 03:17:25 -0500

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,32 @@
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ENT.PrintName = "Watermelon SCP"
+ENT.Author = "Craig D"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
+ENT.Category = "Fun"
+
+function ENT:Initialize()
+    if SERVER then
+        self:SetModel("models/props_junk/watermelon01.mdl")
+        self:PhysicsInit(SOLID_VPHYSICS)
+        self:SetMoveType(MOVETYPE_VPHYSICS)
+        self:SetSolid(SOLID_VPHYSICS)
+
+        local phys = self:GetPhysicsObject()
+        if IsValid(phys) then phys:Wake() end
+
+        self:SetTrigger(true)
+        self:SetUseType(SIMPLE_USE)
+    end
+end
+
+if SERVER then
+    function ENT:StartTouch(ent)
+        if not IsValid(ent) or not ent:IsPlayer() then return end
+        if ent:GetNWBool("WMCursed", false) then return end
+        if WMCurse and WMCurse.InfectPlayer then
+            WMCurse.InfectPlayer(ent, self)
+        end
+    end
+end

64d06bf6d4ad422aaa19dd40688e6de5988d73c2
Craigy131 <65000316+Trey16Miller@users.noreply.github.com>
2026-01-19 03:18:56 -0500

    Implement WMCurse green screen effect

---
+++ wmcurse_cl.lua
@@ -0,0 +1,22 @@
+local wm_on = false
+
+net.Receive("WMCurse_Set", function()
+    wm_on = net.ReadBool()
+end)
+
+hook.Add("RenderScreenspaceEffects", "WMCurse_GreenScreen", function()
+    if not wm_on then return end
+
+    local cm = {}
+    cm["$pp_colour_addr"] = 0
+    cm["$pp_colour_addg"] = 0.2
+    cm["$pp_colour_addb"] = 0
+    cm["$pp_colour_brightness"] = -0.05
+    cm["$pp_colour_contrast"] = 1.1
+    cm["$pp_colour_colour"] = 0.2
+    cm["$pp_colour_mulr"] = 0
+    cm["$pp_colour_mulg"] = 0.6
+    cm["$pp_colour_mulb"] = 0
+
+    DrawColorModify(cm)
+end)
